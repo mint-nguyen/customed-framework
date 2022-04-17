c4b58a36b34aae0cf2347ed489a34054760aeff4
mintnguyen.pa <mintnguyen.pa@gmail.com>
2022-04-17 12:40:54 -0700

    feat: json-server

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # How to run
 
 1. npm i
-2. parcel index.html
+2. npm i -g parcel-bundler
+3. npm i -g json-server
+4. json-server -w db.json
+5. parcel index.html
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,12 @@ import { User } from "./models/User";
 const user = new User({ name: "Mint", age: 20 });
 
 console.log(user);
-user.set({ name: "MintNext", age: 21 });
+user.on("change", () => {
+  console.log("Chage 1");
+});
+user.on("save", () => {
+  console.log("Save");
+});
+
+user.trigger("change");
 console.log(user);
